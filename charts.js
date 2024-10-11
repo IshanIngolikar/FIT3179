fd13668245259a221004cbe8cee943fc2229b14c
Ishan Ingolikar <iing0003@student.monash.edu>
2024-10-12 04:53:10 +1100

    pie chart for sector analysis

--- charts.js
+++ charts.js
@@ -6,22 +6,6 @@ var vg_2 = "vega/choropleth.vg.json";
 vegaEmbed("#bedperperson", vg_2, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
-var vg_3 = "js/temperature_anomoly.vg.json";
-vegaEmbed("#temp_anomoly", vg_3, {"actions": false}).then(function(result) {
-}).catch(console.error);
-
-var vg_4 = "js/annual_max_temp.vg.json";
-vegaEmbed("#max_temp", vg_4, {"actions": false}).then(function(result) {
-}).catch(console.error);
-
-var vg_5 = "js/proportion_of_forest_area_burnt.vg.json";
-vegaEmbed("#forest_area", vg_5, {"actions": false}).then(function(result) {
-}).catch(console.error);
-
-var vg_6 = "js/forest_category.vg.json";
-vegaEmbed("#forest_category", vg_6, {"actions": false}).then(function(result) {
-}).catch(console.error);
-
-var vg_7 = "js/isotype_grid.vg.json";
-vegaEmbed("#respiratory", vg_7,{"actions": false}).then(function(result) {
+var vg_3 = "vega/pie.vg.json";
+vegaEmbed("#sectoranalysis", vg_3, {"actions": false}).then(function(result) {
 }).catch(console.error);
